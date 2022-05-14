56da5d154a28a20b9d9801510088db305118c9f8
Alfredo Perez <alfredo.perez.q@gmail.com>
2022-05-14 17:18:57 -0500

    adds logging

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -30,6 +30,8 @@ const Giscus = () => {
       lang,
     } = siteMetadata?.comment?.giscusConfig
 
+    console.log({ repositoryId, repo })
+
     const script = document.createElement('script')
     script.src = 'https://giscus.app/client.js'
     script.setAttribute('data-repo', repo)
