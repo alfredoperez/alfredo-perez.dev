82d21b8d68c3a53a5add83bc8e37a1bf26ed7c25
Alfredo Perez <alfredo.perez.q@gmail.com>
2022-07-11 15:43:31 -0500

    Notes Updates (#9)
    * Auto stash before merge of "main" and "origin/main"
    
    * Fixing error

--- libs/ui/LayoutWrapper.js
+++ libs/ui/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo from '@/data/logo-A.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -15,7 +15,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Logo style={{ width: '50px', height: '50px' }} />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
